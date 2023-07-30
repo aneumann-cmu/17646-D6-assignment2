4e9b41f03629174a5866ef71ab9b6283acf88c50
Arthur N <arthur.neumann8@gmail.com>
2023-07-30 00:16:57 -0700

    setup_env updates

--- setup_env.sh
+++ setup_env.sh
@@ -1,37 +1,6 @@
-#!/bin/bash
-
-# Update system packages
-sudo apt update
+#! /bin/bash
 sudo apt upgrade -y
-
-# Install required packages
-sudo apt install -y \
-    git \
-    cpu-checker \
-    qemu-kvm \
-    virt-manager \
-    libvirt-daemon-system \
-    virtinst \
-    libvirt-clients \
-    bridge-utils \
-    qemu \
-    libvirt-daemon-system \
-    libvirt-clients \
-    libxslt-dev \
-    libxml2-dev \
-    libvirt-dev \
-    zlib1g-dev \
-    ruby-dev \
-    ruby-libvirt \
-    ebtables \
-    dnsmasq-base
-
-# Add HashiCorp GPG key
+sudo apt install -y git virtualbox
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-# Add HashiCorp repository to sources
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-# Update and install Vagrant
-sudo apt update
-sudo apt install vagrant
+sudo apt update && sudo apt install vagrant
