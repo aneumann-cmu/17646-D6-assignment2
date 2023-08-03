6df3c05835273e9009909ad4586bdc857ba476ac
Arthur N <arthur.neumann8@gmail.com>
2023-08-03 06:10:43 -0700

    deploy mostly working

--- devops-pl.sh
+++ devops-pl.sh
@@ -8,3 +8,12 @@ download_jar_file() {
 # Setup Jenkins/SonarQube/PostGRE containers
 echo "Setting up container environment"
 docker-compose up -d
+docker exec jenkins /usr/sbin/sshd
+docker exec jenkins ansible-playbook /root/ansible_config/generate_ssh_keys.yaml
+docker exec pcapp ansible-playbook /root/ansible_config/generate_ssh_keys.yaml
+docker exec pcapp ansible-playbook /root/ansible_config/generate_authorized_keys.yaml
+docker exec jenkins ansible-playbook /root/ansible_config/generate_authorized_keys.yaml
+docker exec jenkins ansible-playbook /var/jenkins_home/sonar_config/deploy_sonarqube.yaml -u jenkins
+docker exec jenkins ansible-playbook /var/jenkins_home/jenkins_config/configure_jenkins.yaml -u jenkins
+docker exec jenkins ansible-playbook /var/jenkins_home/jenkins_config/deploy_jenkins.yaml -u jenkins
+docker exec jenkins ansible-playbook /var/jenkins_home/jenkins_config/pc_build.yaml -u jenkins
